b39a3ab1f27513fd104bc2cadfcaca2d6604be52
Chris Desjardins <git@chrisd.info>
2015-04-25 15:04:13 +0200

    Add quick/dirty delta timer

---
+++ deltatimer.h
@@ -0,0 +1,26 @@
+#ifndef DELTA_TIMER_Hxx
+#define DELTA_TIMER_Hxx
+class DeltaTimer
+{
+public:
+    DeltaTimer() : _beg(HRClock::now())
+    {
+    }
+
+    void reset()
+    {
+        _beg = HRClock::now();
+    }
+
+    double elapsed() const
+    {
+        return std::chrono::duration_cast<std::chrono::milliseconds>
+                   (HRClock::now() - _beg).count();
+    }
+
+private:
+    typedef std::chrono::high_resolution_clock HRClock;
+    std::chrono::time_point<HRClock> _beg;
+};
+
+#endif
