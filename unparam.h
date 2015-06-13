4fdeb096610bb123b85a8917cb66dc16df7b1264
Chris Desjardins <git@chrisd.info>
2015-06-13 14:13:17 +0200

    Fix compiler warning on linux

--- unparam.h
+++ unparam.h
@@ -1,7 +1,10 @@
 
 #ifndef UNREF_PARAM
 
-
+#ifndef WIN32
+#define UNREF_PARAM(expr) do { (void)(expr); } while (0)
+#else
 #define UNREF_PARAM(expr) do { (void)(expr); } while (0,0)
+#endif
 
 #endif
